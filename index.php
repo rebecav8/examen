72c137a70500f9d05bb959e0c3339b39906ce9d4
axeluuxx <162248978+axeluuxx@users.noreply.github.com>
2024-03-04 19:17:48 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-
+# examen
+<?php
+//marca
+$nike = [
+//caracteristicas
+   "modelo" => "air force 1",
+   "color" => "blanco y negro",
+  "precio" => "110dlls",
+    "talla" => "8.5 usa",
+  "año creado" => "1982",
+   "diseñador" => "Bruce Kilgore",
+  "tienda" => "nike",
+  "color mas bendido" => "blanco",
+  "donde fue creado" => "Nueva York, y más en concreto -Harlem. ",
+   "que tipo de tenis son" => "son zapatillas deportivas robustas y su suela presenta una plataforma elevada y curvada para brindar un buen soporte en el juego.",
+];
+echo json_encode($nike);
