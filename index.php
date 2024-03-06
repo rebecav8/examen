07c24c390e23562d290d95bed698369350d09ee0
rebecav8 <162385783+rebecav8@users.noreply.github.com>
2024-03-05 18:19:11 -0800

    Update index.php

--- index.php
+++ index.php
@@ -14,4 +14,19 @@ $nike = [
   "donde fue creado" => "Nueva York, y más en concreto -Harlem. ",
    "que tipo de tenis son" => "son zapatillas deportivas robustas y su suela presenta una plataforma elevada y curvada para brindar un buen soporte en el juego.",
 ];
+<?php
+//marca
+$dior = [
+//caracteristicas
+   "modelo" => "dior b23",
+   "color" => "blanco,negro,azul,naranja,gris",
+  "precio" => "mxn 22,520.00",
+    "talla" => "39,40,41",
+  "año creado" => "2018",
+   "diseñador" => "kim Jones",
+  "tienda" => "dior",
+  "color mas bendido" => "gis",
+  "donde fue creado" => "Italia",
+   "que tipo de tenis son" => "zapatillas altas y bajas de lona con esrilo fashoin.",
+];
 echo json_encode($nike);
